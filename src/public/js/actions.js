85d0cf02e833004b62dec05df49c838e5e741ac7
Oleg Strelnikov <olegstr08@gmail.com>
2024-11-24 02:28:18 +0400

    feat(darts0): добавил проверку на вводимое число очков при результате броска игрока (не больше 20 с удвоением или утроением)

--- src/public/js/actions.js
+++ src/public/js/actions.js
@@ -8,7 +8,6 @@ export default class Actions {
         let scores = ''
         let scoresQuantity
         do {
-
             scores = prompt(`Результат ${moveNumber}-го броска игрока ${player.name}`)
             scoresQuantity = Number(scores)
             const [symbol, ...numberArr] = scores;
@@ -18,26 +17,42 @@ export default class Actions {
                 case 'T':
                     if (Number(number)) {
                         // return parseInt(number) * 3;
-                        return [parseInt(number)*3, 'triple']
+                        // return [parseInt(number) * 3, 'triple']
+                        if (parseInt(number) <= 20) {
+                            return [parseInt(number) * 3, 'triple']
+                        } else {
+                            scoresQuantity = NaN
+                        }
                     } else {
-                        alert('error')
+                        alert('error! Нужно ввести только число от 0 до 20, для утроения используйте t или T в начале строки, для удвоения w или W');
                         continue;
                     }
                 case 'w':
                 case 'W':
                     if (Number(number)) {
                         // return parseInt(number) * 2;
-                        return [parseInt(number)*2, 'double']
+                        // return [parseInt(number) * 2, 'double']
+                        if (parseInt(number) <= 20) {
+                            return [parseInt(number) * 2, 'double']
+                        } else {
+                            scoresQuantity = NaN
+                        }
                     } else {
-                        alert('error')
+                        alert('error! Нужно ввести только число от 0 до 20, для утроения используйте t или T в начале строки, для удвоения w или W');
                         continue;
                     }
                 default:
-                    if (Number(scoresQuantity)) {
+                    if (Number(scoresQuantity>=0)) {
                         // return parseInt(scoresQuantity);
-                        return [parseInt(scoresQuantity), 'normal']
+                        // return [parseInt(scoresQuantity), 'normal']
+                        if (parseInt(scoresQuantity) <= 20) {
+                            return [parseInt(scoresQuantity), 'normal']
+                        } else {
+                            alert('error! Нужно ввести только число от 0 до 20, для утроения используйте t или T в начале строки, для удвоения w или W');
+                            scoresQuantity = NaN
+                        }
                     } else {
-                        alert('error!');
+                        alert('error! Нужно ввести только число от 0 до 20, для утроения используйте t или T в начале строки, для удвоения w или W');
                     }
 
             }
@@ -47,16 +62,16 @@ export default class Actions {
     }
 
     calculateScores(player, moveResult, doubling) {
-        let result =  player.scores - moveResult
-    
-        if (result >1 || (result === 0 && doubling === 'double')) {
+        let result = player.scores - moveResult
+
+        if (result > 1 || (result === 0 && doubling === 'double')) {
             return result
         } else {
             alert('error Перебор')
             return player.scores
         }
 
-        
+
     }
 
     residueMessage(player) {
