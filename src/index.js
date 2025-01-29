423a4910d6680f91f77d0881bb832a3557494ca6
Oleg Strelnikov <olegstr08@gmail.com>
2025-01-29 21:58:58 +0400

    refactor(index.js): заменил функцию listener на переменную

--- src/index.js
+++ src/index.js
@@ -5,21 +5,14 @@ import { startData } from "./public/js/startData";
 
 document.addEventListener('DOMContentLoaded', () => {
     const data = startData.getData()
-    // function listener() {
-    //         console.log('Ураааа !');
-    //         (new gameClass(data)).game()
-    //     }
+   
     const btn = document.querySelector('.play')
-    function listener(event){
+    const listener = (event) => {
         (new gameClass(data)).game()
-        btn.removeEventListener('click',listener)
-    }   
+        btn.removeEventListener('click', listener)
+    }
 
     btn.addEventListener('click', listener)
-    // btn.addEventListener('click', listener)
-    // btn.addEventListener('click', ()=>{
-    //     btn.removeEventListener('click', listener)
-    // })
-
+  
 })
 
